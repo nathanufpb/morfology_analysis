4dd35657a10d13b5279cfeb4e587ab0c5e5ce8fc
Nathan Paiva Brito <160909180+nathanufpb@users.noreply.github.com>
2026-03-05 15:50:14 -0300

    refactor(init): add __all__ exports to all __init__.py modules (closes #14) (#22)

--- delta_phylo/phylogeny/__init__.py
+++ delta_phylo/phylogeny/__init__.py
@@ -1,7 +1,15 @@
 """Phylogeny subpackage for phylogenetic analyses."""
 
 from delta_phylo.phylogeny.parsimony import ParsimonyAnalysis
-from delta_phylo.phylogeny.distance import DistanceAnalysis
+from delta_phylo.phylogeny.distance import DistanceAnalysis, gower_distance
+from delta_phylo.phylogeny.likelihood import LikelihoodAnalysis, mk_likelihood
 from delta_phylo.phylogeny.tree_utils import TreeUtils
 
-__all__ = ["ParsimonyAnalysis", "DistanceAnalysis", "TreeUtils"]
+__all__ = [
+    "ParsimonyAnalysis",
+    "DistanceAnalysis",
+    "gower_distance",
+    "LikelihoodAnalysis",
+    "mk_likelihood",
+    "TreeUtils",
+]
