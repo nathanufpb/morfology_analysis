fc341a42dc42131a19e5233b6b9edce5634b0242
nathan.paiva <nathan.paiva@academico.ufpb.br>
2026-03-05 15:50:00 -0300

    refactor(init): add __all__ exports to all __init__.py modules (closes #14)

--- delta_phylo/__init__.py
+++ delta_phylo/__init__.py
@@ -3,8 +3,65 @@ delta-phylo: Morphological phylogenetic analyses using DELTA format datasets.
 
 A modern Python library for systematists, taxonomists, and evolutionary
 biologists working with morphological characters and DELTA data.
+
+Quick start::
+
+    from delta_phylo import DeltaReader, MatrixBuilder, MorphologicalMatrix
+    from delta_phylo import NexusWriter
+    from delta_phylo import ParsimonyAnalysis, DistanceAnalysis
 """
 
 from delta_phylo._version import __version__
 
-__all__ = ["__version__"]
+# Core parser
+from delta_phylo.parser import DeltaReader, Character, CharacterType, Taxon, CharacterState
+
+# Matrix
+from delta_phylo.matrix import MatrixBuilder, MorphologicalMatrix, MatrixCleaner, MatrixEncoder
+
+# IO writers
+from delta_phylo.io import NexusWriter, PhylipWriter, TNTWriter, CSVWriter
+
+# Phylogenetic analyses
+from delta_phylo.phylogeny import (
+    ParsimonyAnalysis,
+    DistanceAnalysis,
+    gower_distance,
+    LikelihoodAnalysis,
+    mk_likelihood,
+    TreeUtils,
+)
+
+# Utilities
+from delta_phylo.utils import validate_delta_directory, validate_matrix, get_logger
+
+__all__ = [
+    "__version__",
+    # parser
+    "DeltaReader",
+    "Character",
+    "CharacterType",
+    "Taxon",
+    "CharacterState",
+    # matrix
+    "MatrixBuilder",
+    "MorphologicalMatrix",
+    "MatrixCleaner",
+    "MatrixEncoder",
+    # io
+    "NexusWriter",
+    "PhylipWriter",
+    "TNTWriter",
+    "CSVWriter",
+    # phylogeny
+    "ParsimonyAnalysis",
+    "DistanceAnalysis",
+    "gower_distance",
+    "LikelihoodAnalysis",
+    "mk_likelihood",
+    "TreeUtils",
+    # utils
+    "validate_delta_directory",
+    "validate_matrix",
+    "get_logger",
+]
--- delta_phylo/cli/__init__.py
+++ delta_phylo/cli/__init__.py
@@ -1,1 +1,5 @@
 """CLI subpackage for the delta-phylo command line interface."""
+
+from delta_phylo.cli.main import cli
+
+__all__ = ["cli"]
